4f57563d34a27be7f3108774a97131c4ffdf2369
Leo_Alves <89089486+1LeoAlves@users.noreply.github.com>
2024-02-20 17:46:24 -0300

    Base and End push

---
+++ FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		private static List<DogViewModel> dogs = new List<DogViewModel>();
+		public IActionResult Index()
+		{
+			return View(dogs);
+		}
+		public string Hello()
+		{
+			return "Who's there?";
+		}
+		public IActionResult CreateDog(DogViewModel dogViewModel)
+		{
+			//return View("Index");
+			dogs.Add(dogViewModel);
+			return RedirectToAction(nameof(Index));
+		}
+		public IActionResult Create()
+		{
+			var dogVM = new DogViewModel();
+			return View(dogVM);
+		}
+
+    }
+}
